37727ae55c826d6092919d2f6510bf4fdac87ecc
Ron Maina <ronmwangi00@gmail.com>
2024-06-19 15:06:20 +0300

    Added price to cart model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from .extensions import db
 from flask_bcrypt import Bcrypt
-# from sqlalchemy_serializer import SerializerMixin
 
 
 from sqlalchemy.ext.hybrid import hybrid_property
@@ -17,24 +16,24 @@ class Users(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
     role = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    # _password_hash = db.Column(db.String, nullable = False)
+    _password_hash = db.Column(db.String, nullable = False)
 
     my_cart = db.relationship("Cart", backref = 'user')
 
-    # @hybrid_property
-    # def password_hash(self):
-    #     return 'Unauthorized'
+    @hybrid_property
+    def password_hash(self):
+        return 'Unauthorized'
     
-    # @password_hash.setter
-    # def password_hash(self, password):
-    #     password_hash = bcrypt.generate_password_hash(
-    #         password.encode('utf-8')
-    #     )
-    #     self._password_hash = password_hash.decode('utf-8')
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
 
-    # def authenticate(self, password):
-    #     return bcrypt.check_password_hash(
-    #         self._password_hash, password.encode('utf-8'))
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
 
 
 
@@ -44,6 +43,7 @@ class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String)
     product_id = db.Column(db.Integer)
+    price = db.Column(db.Integer)
     payment_status = db.Column(db.Boolean, default=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
@@ -53,6 +53,7 @@ class Cart(db.Model):
             'id': self.id,
             'product_name': self.product_name,
             'product_id': self.product_id,
+            'price': self.price,
             'payment_status': self.payment_status,
             'user_id': self.user_id
         }
